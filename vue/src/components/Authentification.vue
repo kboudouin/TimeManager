df7653eb0758ac0b196fa3b19e85af9a5d0f3eb5
kieran <kieran.boudouin@epitech.eu>
2023-10-30 00:52:13 +0100

    deploy

--- vue/src/components/Authentification.vue
+++ vue/src/components/Authentification.vue
@@ -5,7 +5,7 @@ import { useRouter, useRoute } from "vue-router";
 import { useToast } from "vue-toast-notification";
 import "vue-toast-notification/dist/theme-sugar.css";
 
-const showSideNav = ref(true);
+const showSideNav = ref(false);
 
 const toggleSideNav = () => {
   showSideNav.value = !showSideNav.value;
