6cece853270d1e20e3a6b7091c5b4c02cf07f91a
kieran <kieran.boudouin@epitech.eu>
2023-10-30 11:13:53 +0100

    deploy

--- api/lib/api_web/endpoint.ex
+++ api/lib/api_web/endpoint.ex
@@ -47,6 +47,6 @@ defmodule ApiWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
-  plug Corsica, origins: "*", allow_headers: ["content-type"], allow_methods: [:get, :post, :delete, :put]
+  plug Corsica, origins: "*",allow_methods: [:get, :post, :delete, :put]
   plug ApiWeb.Router
 end
