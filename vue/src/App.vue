736f07dac2f0bb6ba9bc1b05aea61c37cf58fd87
kieran <kieran.boudouin@epitech.eu>
2023-10-31 11:33:31 +0100

    deploy

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -8,7 +8,6 @@ onMounted(() => {
 
 const isUserConnected = ref(localStorage.getItem("userId"));
 </script>
-
 <template>
   <Authentification />
   <div><router-view v-if="isUserConnected"></router-view></div>
