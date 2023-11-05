89ec115cbcc18fd6e57e30c4b62a184a45ee448d
kieran <kieran.boudouin@epitech.eu>
2023-11-05 03:24:57 +0100

    Jwt Header

--- api/lib/api_web/guardian_error_handler.ex
+++ api/lib/api_web/guardian_error_handler.ex
@@ -1,11 +1,18 @@
 defmodule ApiWeb.GuardianErrorHandler do
   import Plug.Conn
 
-    def auth_error(conn, {:invalid_token, _reason}, _opts) do
+  def auth_error(conn, {:invalid_token, _reason}, _opts) do
     conn
     |> put_status(:unauthorized)
     |> put_resp_header("content-type", "application/json")
     |> send_resp(:unauthorized, "{\"error\":\"Invalid token\"}")
+  end
+
+  def auth_error(conn, _error, _opts) do
+    conn
+    |> put_status(:unauthorized)
+    |> put_resp_header("content-type", "application/json")
+    |> send_resp(:unauthorized, "{\"error\":\"Unauthorized\"}")
   end
 
 end
