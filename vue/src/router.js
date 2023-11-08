926fb57bc916f1bcb0d7b328a3e7c001033704ec
kieran <kieran.boudouin@epitech.eu>
2023-11-08 11:18:15 +0100

    Landing pageé

--- vue/src/router.js
+++ vue/src/router.js
@@ -6,9 +6,10 @@ import Profile from "./components/Profile.vue";
 import Teams from "./components/Teams.vue";
 import error from "./components/notAuthorised.vue";
 import wtRender from "./components/workingTimes/wtRender.vue";
+import Landing from "./components/Landing.vue";
 
 const routes = [
-  { path: "/", name: "index" },
+  { path: "/", name: "landing", component: Landing },
   { path: "/error", name: "error", component: error },
   { path: "/Admin", name: "Admin", component: Admin },
   { path: "/Dashboard/:id", name: "chartManager", component: ChartManager },
