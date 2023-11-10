2bb41aefe7ae37548cd5867a125442527152ead2
wijdanepitech <148757893+wijdanepitech@users.noreply.github.com>
2023-11-10 15:00:34 +0100

    essaye carousel team

--- vue/src/components/Teams.vue
+++ vue/src/components/Teams.vue
@@ -1,102 +1,124 @@
 <template>
-  <div>
-    <h1>TEAM</h1>
-    <h1 class="text-3xl font-semibold mb-4">Gestion des équipes</h1>
+  <button @click="toggleAddModal" class="btn btn-success fixed top-4 right-32">
+    Create Team
+  </button>
 
-    <!-- Section "Mes équipes" -->
-    <h2 class="text-xl font-semibold mb-4">Mes équipes</h2>
-
-    <div class="carousel rounded-box">
-  <div class="carousel-item">
-    <div class="flex flex-col w-full lg:flex-row">
-  <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div> 
-  <div class="divider lg:divider-horizontal">OR</div> 
-  <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div>
-</div>
-
-  </div> 
-  <div class="carousel-item">
-    <div class="flex flex-col w-full lg:flex-row">
-  <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div> 
-  <div class="divider lg:divider-horizontal">OR</div> 
-  <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div>
-</div>
-
-  </div> 
-
-  <div class="carousel-item">
-    <div class="flex flex-col w-full lg:flex-row">
-  <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div> 
-  <div class="divider lg:divider-horizontal">OR</div> 
-  <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div>
-</div>
-
-  </div> 
-  <div class="carousel-item">
-    <div class="flex flex-col w-full lg:flex-row">
-  <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div> 
-  <div class="divider lg:divider-horizontal">OR</div> 
-  <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div>
-</div>
-
-  </div> 
-  <div class="carousel-item">
-    <div class="flex flex-col w-full lg:flex-row">
-  <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div> 
-</div>
-
-  </div> 
-  <div class="carousel-item">
-    <div class="flex flex-col w-full lg:flex-row">
-  <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div> 
-</div>
-  </div> 
-  <div class="carousel-item">
-    <div class="flex flex-col w-full lg:flex-row">
-  <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div> 
-</div>
+  <div v-if="toggleAdd">
+    <addTeamModal @close="toggleAddModal" @fetchData="fetchData" />
   </div>
-</div>
 
-
+  <div class="relative overflow-x-auto shadow-md sm:rounded-lg z-10 mt-12">
+    <carousel>
+      <carousel-item v-for="group in chunkedTeams" :key="group[0].id">
+        <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead
+            class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+          >
+            <tr>
+              <th scope="col" class="px-6 py-3">Manager</th>
+              <th scope="col" class="px-6 py-3">Members</th>
+              <th scope="col" class="px-6 py-3">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+              v-for="team in group"
+              :key="team.id"
+            >
+              <td class="px-6 py-4">
+                <span
+                  class="bg-yellow-100 text-yellow-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-yellow-900 dark:text-yellow-300"
+                >{{ team.leader }}</span
+                >
+              </td>
+              <td class="px-6 py-4">
+                <span
+                  class="bg-blue-100 text-blue-800 text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300"
+                  v-for="member in team.members"
+                  :key="member.id"
+                >{{ member.user }}</span
+                >
+              </td>
+              <td class="px-6 py-4">
+                <button @click="deleteTeam(team.id)" class="btn btn-xs btn-error">
+                  Delete
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </carousel-item>
+    </carousel>
   </div>
 </template>
 
-<script>
+<script setup>
+import { ref, watch, onMounted } from "vue";
+import addTeamModal from "./adminTeamAdd.vue";
+import { useToast } from "vue-toast-notification";
+import axios from "axios";
+import { defineProps, defineEmits } from "vue";
+import { Carousel, CarouselItem } from "daisyui";
 
+const teams = ref(null);
+const toggleAdd = ref(false);
 
-export default {
-  components: {
- 
-  },
-  data() {
-    return {
-      userUsername: null,
-      id: null,
-      EMAIL: null,
-      selectedMembers: [],
-      LIST: [],
-      items: [
-        { text: 'test testte 1' },
-        { text: 'test testte 2' },
-        { text: 'test testte 3' },
-        { text: 'test testte 4' },
-        { text: 'test testte 5' },
-        { text: 'test testte 6' },
-        { text: 'test testte 7' },
-      ],
-
-    };
-  },
-  created() {
-    // Récupérez la valeur depuis le localStorage
-    this.userUsername = localStorage.getItem('userUsername');
-    this.id = localStorage.getItem('userId');
-    this.EMAIL = localStorage.getItem('userEmail');
-  },
+const fetchData = async () => {
+  try {
+    const token = localStorage.getItem("token");
+    const resp = await axios.get(`https://epitechproject.com/api/teams`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    if (resp.data.teams) {
+      teams.value = resp.data.teams;
+    }
+  } catch (error) {
+    const $toast = useToast();
+    $toast.error("Error fetching all teams!");
+    console.error(error);
+  }
 };
+
+const deleteTeam = async (id) => {
+  try {
+    const token = localStorage.getItem("token");
+    await axios.delete(`https://epitechproject.com/api/teams/${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const $toast = useToast();
+    $toast.success("Team Deleted!");
+    fetchData();
+  } catch (error) {
+    const $toast = useToast();
+    $toast.error("Error deleting team!");
+    console.error(error);
+  }
+};
+
+const toggleAddModal = async () => {
+  toggleAdd.value = !toggleAdd.value;
+};
+
+onMounted(() => {
+  fetchData();
+});
+
+const chunkedTeams = ref([]);
+
+watch(teams, () => {
+  chunkedTeams.value = chunkArray(teams.value, 3);
+});
+
+function chunkArray(array, chunkSize) {
+  const result = [];
+  for (let i = 0; i < array.length; i += chunkSize) {
+    result.push(array.slice(i, i + chunkSize));
+  }
+  return result;
+}
 </script>
-
-<style>
-
-</style>
