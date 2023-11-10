7cf126733a939a51441d2f2a7d33500ace088042
wijdanepitech <148757893+wijdanepitech@users.noreply.github.com>
2023-11-10 10:06:03 +0100

    retry carousel

--- vue/src/components/Teams.vue
+++ vue/src/components/Teams.vue
@@ -1,48 +1,28 @@
 <template>
-  <h1> TEAM</h1>
-
+  <div>
+    <h1>TEAM</h1>
     <h1 class="text-3xl font-semibold mb-4">Gestion des équipes</h1>
 
     <!-- Section "Mes équipes" -->
+    <h2 class="text-xl font-semibold mb-4">Mes équipes</h2>
 
-      <h2 class="text-xl font-semibold mb-4">Mes équipes</h2>
-
-      <div class="carousel carousel-end rounded-box">
-  <div class="carousel-item">
-    <p> test testte </p>
-    <img src="/images/stock/photo-1559703248-dcaaec9fab78.jpg" alt="Drink" />
-  </div> 
-  <div class="carousel-item">
-    <p> test testte </p>
-    <img src="/images/stock/photo-1565098772267-60af42b81ef2.jpg" alt="Drink" />
-  </div> 
-  <div class="carousel-item">
-    <p> test testte </p>
-    <img src="/images/stock/photo-1572635148818-ef6fd45eb394.jpg" alt="Drink" />
-  </div> 
-  <div class="carousel-item">
-    <p> test testte </p>
-    <img src="/images/stock/photo-1494253109108-2e30c049369b.jpg" alt="Drink" />
-  </div> 
-  <div class="carousel-item">
-    <p> test testte </p>
-    <img src="/images/stock/photo-1550258987-190a2d41a8ba.jpg" alt="Drink" />
-  </div> 
-  <div class="carousel-item">
-    <p> test testte </p>
-    <img src="/images/stock/photo-1559181567-c3190ca9959b.jpg" alt="Drink" />
-  </div> 
-  <div class="carousel-item">
-    <p> test testte </p>
-    <img src="/images/stock/photo-1601004890684-d8cbf643f5f2.jpg" alt="Drink" />
+    <!-- Use the carousel component -->
+    <Carousel :autoplay="true" :autoplaySpeed="3000">
+      <div v-for="(item, index) in items" :key="index" class="carousel-item">
+        <p>{{ item.text }}</p>
+      </div>
+    </Carousel>
   </div>
-</div>
-    
-  
 </template>
 
 <script>
+import Carousel from 'vue-slick-carousel';
+import 'vue-slick-carousel/dist/vue-slick-carousel.css';
+
 export default {
+  components: {
+    Carousel,
+  },
   data() {
     return {
       userUsername: null,
@@ -50,13 +30,26 @@ export default {
       EMAIL: null,
       selectedMembers: [],
       LIST: [],
+      items: [
+        { text: 'test testte 1' },
+        { text: 'test testte 2' },
+        { text: 'test testte 3' },
+        { text: 'test testte 4' },
+        { text: 'test testte 5' },
+        { text: 'test testte 6' },
+        { text: 'test testte 7' },
+      ],
     };
   },
   created() {
     // Récupérez la valeur depuis le localStorage
-    this.userUsername = localStorage.getItem("userUsername");
-    this.id = localStorage.getItem("userId");
-    this.EMAIL = localStorage.getItem("userEmail");
+    this.userUsername = localStorage.getItem('userUsername');
+    this.id = localStorage.getItem('userId');
+    this.EMAIL = localStorage.getItem('userEmail');
   },
 };
 </script>
+
+<style>
+/* Add any custom styles for your carousel here */
+</style>
