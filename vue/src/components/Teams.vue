5502a43df3ab2a73b617938e8d0c6d71819adf6d
wijdanepitech <148757893+wijdanepitech@users.noreply.github.com>
2023-11-10 16:44:52 +0100

    fix

--- vue/src/components/Teams.vue
+++ vue/src/components/Teams.vue
@@ -9,7 +9,12 @@
     <div class="carousel rounded-box">
       <div class="carousel-item" v-for="team in teams" :key="team.id">
         <div class="flex flex-col w-full lg:flex-row">
-          <div v-for="index in 3" :key="index" class="grid flex-grow h-80 card bg-base-300 rounded-box place-items-center">
+          <div v-for="index in 3" :key="index" class="relative group grid flex-grow h-80 card bg-base-300 rounded-box place-items-center">
+            <div class="absolute top-2 left-2 cursor-pointer text-red-500 opacity-0 group-hover:opacity-100 transition-opacity">
+              <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+              </svg>
+            </div>
             <template v-if="team.members">
               <h1>Equipe ID: {{ team.id }}</h1>
               <h2>Leader: {{ team.leader }}</h2>
