43f4a61d3061c5496bbfef3c478ea17ac924c9d5
wijdanepitech <148757893+wijdanepitech@users.noreply.github.com>
2023-11-10 10:32:05 +0100

    test

--- vue/src/components/Teams.vue
+++ vue/src/components/Teams.vue
@@ -43,23 +43,17 @@
   <div class="carousel-item">
     <div class="flex flex-col w-full lg:flex-row">
   <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div> 
-  <div class="divider lg:divider-horizontal">OR</div> 
-  <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div>
 </div>
 
   </div> 
   <div class="carousel-item">
     <div class="flex flex-col w-full lg:flex-row">
   <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div> 
-  <div class="divider lg:divider-horizontal">OR</div> 
-  <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div>
 </div>
   </div> 
   <div class="carousel-item">
     <div class="flex flex-col w-full lg:flex-row">
   <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div> 
-  <div class="divider lg:divider-horizontal">OR</div> 
-  <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div>
 </div>
   </div>
 </div>
