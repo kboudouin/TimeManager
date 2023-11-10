38ffffa0f58961cac09f24478004d5e3800da4f1
wijdanepitech <148757893+wijdanepitech@users.noreply.github.com>
2023-11-10 17:08:09 +0100

    fix

--- vue/src/components/Teams.vue
+++ vue/src/components/Teams.vue
@@ -69,7 +69,7 @@ onMounted(() => {
       <div class="carousel-item" v-for="team in teams" :key="team.id">
         <div class="flex flex-col w-full lg:flex-row">
           <div class="relative group grid flex-grow h-80 card bg-base-300 rounded-box place-items-center">
-            <div class="absolute top-2 left-2 cursor-pointer text-red-500 opacity-0 group-hover:opacity-100 transition-opacity" @click="deleteTeam(team.id)">
+            <div class="absolute top-2 left-2 cursor-pointer text-red-500 opacity-0 group-hover:opacity-100 transition-opacity">
               <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
               </svg>
@@ -97,25 +97,14 @@ export default {
         { id: 1, leader: 'John Doe', members: 'membres1' },
         { id: 2, leader: 'Jane Doe', members: 'membres2' },
         { id: 3, leader: 'Doe Doe', members: 'membres3' },
-        // Add more teams as needed
+     
       ],
     };
   },
   created() {
-    // Récupérez la valeur depuis le localStorage
     this.userUsername = localStorage.getItem('userUsername');
     this.id = localStorage.getItem('userId');
     this.EMAIL = localStorage.getItem('userEmail');
-  },
-  methods: {
-    getDividers(team) {
-      // You can modify this function to select dividers based on your logic.
-      return [
-        { id: team.id, leader: team.leader, members: team.members },
-        { id: team.id + 1, leader: team.leader + '1', members: team.members + '1' },
-        { id: team.id + 2, leader: team.leader + '2', members: team.members + '2' },
-      ];
-    },
   },
 };
 </script>
