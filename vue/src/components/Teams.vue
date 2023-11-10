40026e86a158ee958840c380ee5878c78992e984
wijdanepitech <148757893+wijdanepitech@users.noreply.github.com>
2023-11-10 16:50:47 +0100

    fix front

--- vue/src/components/Teams.vue
+++ vue/src/components/Teams.vue
@@ -9,20 +9,21 @@
     <div class="carousel rounded-box">
       <div class="carousel-item" v-for="team in teams" :key="team.id">
         <div class="flex flex-col w-full lg:flex-row">
-          <div v-for="index in 3" :key="index" class="relative group grid flex-grow h-80 card bg-base-300 rounded-box place-items-center">
-            <div class="absolute top-2 left-2 cursor-pointer text-red-500 opacity-0 group-hover:opacity-100 transition-opacity">
-              <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-              </svg>
+          <template v-for="(divider, index) in getDividers(team)" :key="index">
+            <div class="relative group grid flex-grow h-80 card bg-base-300 rounded-box place-items-center">
+              <div class="absolute top-2 left-2 cursor-pointer text-red-500 opacity-0 group-hover:opacity-100 transition-opacity">
+                <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                </svg>
+              </div>
+              <template v-if="divider.members">
+                <h1>Equipe ID: {{ divider.id }}</h1>
+                <h2>Leader: {{ divider.leader }}</h2>
+                <h2>Membres: {{ divider.members }}</h2>
+              </template>
             </div>
-            <template v-if="team.members">
-              <h1>Equipe ID: {{ team.id }}</h1>
-              <h2>Leader: {{ team.leader }}</h2>
-              <h2>Membres: {{ team.members }}</h2>
-            </template>
-            <div class="divider lg:divider-horizontal"></div>
-          </div>
-          <div v-if="index !== 1" class="divider lg:divider-horizontal"></div>
+            <div v-if="index !== 2" class="divider lg:divider-horizontal"></div>
+          </template>
         </div>
       </div>
     </div>
@@ -39,8 +40,9 @@ export default {
       selectedMembers: [],
       LIST: [],
       teams: [
-        { id: 1, leader: 'John Doe', members: 'membres' },
-        { id: 2, leader: 'Jane Doe', members: 'membres' },
+        { id: 1, leader: 'John Doe', members: 'membres1' },
+        { id: 2, leader: 'Jane Doe', members: 'membres2' },
+        { id: 3, leader: 'Doe Doe', members: 'membres3' },
         // Add more teams as needed
       ],
     };
@@ -50,6 +52,16 @@ export default {
     this.userUsername = localStorage.getItem('userUsername');
     this.id = localStorage.getItem('userId');
     this.EMAIL = localStorage.getItem('userEmail');
+  },
+  methods: {
+    getDividers(team) {
+      // You can modify this function to select dividers based on your logic.
+      return [
+        { id: team.id, leader: team.leader, members: team.members },
+        { id: team.id + 1, leader: team.leader + '1', members: team.members + '1' },
+        { id: team.id + 2, leader: team.leader + '2', members: team.members + '2' },
+      ];
+    },
   },
 };
 </script>
