3ab0515eff544797a99293061d8323e1db0c42ad
wijdanepitech <148757893+wijdanepitech@users.noreply.github.com>
2023-11-10 09:50:25 +0100

    test

--- vue/src/components/Teams.vue
+++ vue/src/components/Teams.vue
@@ -32,7 +32,7 @@
     >Previous</span
   >
 </button>
-<!--Carousel controls - next item-->
+<p> ceci est un paragraphe test </p>
 <button
   class="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
   type="button"
