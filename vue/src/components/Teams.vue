e7a907b9fcc6171024c3414ff34bad956e1b085d
wijdanepitech <148757893+wijdanepitech@users.noreply.github.com>
2023-11-10 16:33:03 +0100

    front fix

--- vue/src/components/Teams.vue
+++ vue/src/components/Teams.vue
@@ -9,8 +9,14 @@
     <div class="carousel rounded-box">
   <div class="carousel-item">
     <div class="flex flex-col w-full lg:flex-row">
-  <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div> 
-  <div class="divider lg:divider-horizontal">OR</div> 
+  <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center"> 
+    <h1>Equipe ID</h1>
+    <h2>Leader : John Doe</h2>
+    <h2>Membres : membres</h2>
+    </div> 
+  <div class="divider lg:divider-horizontal"></div> 
+  <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div>
+  <div class="divider lg:divider-horizontal"></div> 
   <div class="grid flex-grow h-32 card bg-base-300 rounded-box place-items-center">content</div>
 </div>
 
