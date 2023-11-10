ade60af352030a397e8c089060947b452c87801a
wijdanepitech <148757893+wijdanepitech@users.noreply.github.com>
2023-11-10 15:58:39 +0100

    fix front

--- vue/src/components/Teams.vue
+++ vue/src/components/Teams.vue
@@ -28,25 +28,25 @@
             </div>
   </div> 
   <div class="carousel-item">
-    <img src="https://etudestech.com/wp-content/uploads/2022/12/entreprise-informatique-scaled.jpeg" alt="Burger" />
+
     <div class="bg-white p-4 rounded">
               <p>Your content here</p>
             </div>
   </div> 
   <div class="carousel-item">
-    <img src="https://etudestech.com/wp-content/uploads/2022/12/entreprise-informatique-scaled.jpeg" alt="Burger" />
+
     <div class="bg-white p-4 rounded">
               <p>Your content here</p>
             </div>
   </div> 
   <div class="carousel-item">
-    <img src="https://etudestech.com/wp-content/uploads/2022/12/entreprise-informatique-scaled.jpeg" alt="Burger" />
+
     <div class="bg-white p-4 rounded">
               <p>Your content here</p>
             </div>
   </div> 
   <div class="carousel-item">
-    <img src="https://etudestech.com/wp-content/uploads/2022/12/entreprise-informatique-scaled.jpeg" alt="Burger" />
+
     <div class="bg-white p-4 rounded">
               <p>Your content here</p>
             </div>
