060e59d8f034c590ceff72f87825e7e95bfb7ba9
wijdanepitech <148757893+wijdanepitech@users.noreply.github.com>
2023-11-10 17:03:52 +0100

    test

--- vue/src/components/Teams.vue
+++ vue/src/components/Teams.vue
@@ -1,3 +1,62 @@
+<script setup>
+import { ref, watch, onMounted } from "vue";
+import addTeamModal from "./adminTeamAdd.vue";
+import { useToast } from "vue-toast-notification";
+import axios from "axios";
+import { defineProps, defineEmits } from "vue";
+
+const teams = ref(null);
+const toggleAdd = ref(false);
+
+const fetchData = async () => {
+  try {
+    const token = localStorage.getItem("token");
+    const resp = await axios.get(`https://epitechproject.com/api/teams`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log;
+    if (resp.data.teams) {
+      teams.value = resp.data.teams;
+    }
+  } catch (error) {
+    const $toast = useToast();
+    $toast.error("Error fetching all teams!");
+    console.error(error);
+  }
+};
+
+
+const deleteTeam = async (id) => {
+  try {
+    const token = localStorage.getItem("token");
+    const resp = await axios.delete(
+      `https://epitechproject.com/api/teams/${id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const $toast = useToast();
+    $toast.success("Team Deleted!");
+    fetchData();
+  } catch (error) {
+    const $toast = useToast();
+    $toast.error("Error deleting team!");
+    console.error(error);
+  }
+};
+const toggleAddModal = async () => {
+  toggleAdd.value = !toggleAdd.value;
+};
+
+onMounted(() => {
+  fetchData();
+});
+</script>
+
 <template>
   <div>
     <h1>TEAM</h1>
@@ -9,21 +68,16 @@
     <div class="carousel rounded-box">
       <div class="carousel-item" v-for="team in teams" :key="team.id">
         <div class="flex flex-col w-full lg:flex-row">
-          <template v-for="(divider, index) in getDividers(team)" :key="index">
-            <div class="relative group grid flex-grow h-80 card bg-base-300 rounded-box place-items-center">
-              <div class="absolute top-2 left-2 cursor-pointer text-red-500 opacity-0 group-hover:opacity-100 transition-opacity">
-                <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-                </svg>
-              </div>
-              <template v-if="divider.members">
-                <h1>Equipe ID: {{ divider.id }}</h1>
-                <h2>Leader: {{ divider.leader }}</h2>
-                <h2>Membres: {{ divider.members }}</h2>
-              </template>
+          <div class="relative group grid flex-grow h-80 card bg-base-300 rounded-box place-items-center">
+            <div class="absolute top-2 left-2 cursor-pointer text-red-500 opacity-0 group-hover:opacity-100 transition-opacity" @click="deleteTeam(team.id)">
+              <svg class="h-4 w-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+              </svg>
             </div>
-            <div v-if="index !== 2" class="divider lg:divider-horizontal"></div>
-          </template>
+            <h1>Equipe ID: {{ team.id }}</h1>
+            <h2>Leader: {{ team.leader }}</h2>
+            <h2>Membres: {{ team.members }}</h2>
+          </div>
         </div>
       </div>
     </div>
